2053bca745778d18734f16d0d78437ac171bb4ed
Monu Rajj <monukumar@monus-MacBook-Pro.local>
2023-12-06 15:12:14 +0530

    completed 1st

--- filereader.py
+++ filereader.py
@@ -1,8 +1,20 @@
 def read_file(filename):
+    sol = []
+    with open("data.txt", "r") as fd:
+        book_list = fd.readlines()
+        for item in book_list:
+            sol.append(item.split(", "))
 
+    return sol
 
 
 def calculate_average_price(books):
+    s=0
+    for i in books:
+        s += i[0]
+    avg = s/len(books)
+    return avg
+
     
 
 
